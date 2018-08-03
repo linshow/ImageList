f550671ef167b070e4b173762577906327dec341
linshow <rb922274532@yahoo.com.tw>
2018-08-03 14:34:27 +0800

    image show imgur picture

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="ui secondary pointing menu">
+        <router-link  to="/" class="active item">
+        image storage 
+        </router-link>
+        <!-- {{ isLoggedIn}} -->
+        <div class="right menu">
+            <div v-if="isLoggedIn" class="horizontal">
+                <router-link to="/" class="item">Galleries</router-link>
+                <router-link class="item" to="/upload">upload</router-link>
+                <a class="item" @click="logout">logout</a>
+               
+              
+            </div>
+
+
+            <a v-else  href="#" class="ui item" @click="login">
+                Login
+            </a>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import {mapActions,mapGetters} from 'vuex';
+export default {
+    name:'AppHeader',
+    computed: mapGetters(['isLoggedIn']),
+    methods: mapActions(['login','logout'])
+};
+</script>
+
+
+<style scoped>
+.horizontal{
+    display: flex;
+    flex-direction: row;
+
+}
+</style>
---
+++ src/store/modules/images.js
@@ -0,0 +1,45 @@
+import api from '../../api/imgur';
+import {router} from '../../main';
+
+const state ={
+    images:[]
+};
+
+const getters = {
+    allImages: state => state.images
+};
+
+
+const actions = {
+   async fetchImages({ rootState,commit }){
+        const {token} = rootState.auth;
+        const response = await api.fetchImages(token);
+        commit('setImages',response.data.data);
+    },
+    async uploadImages({rootState}, images){
+        //get the access token
+        const {token} = rootState.auth;
+
+        //call our Api module to do the upload
+        await api.uploadImages(images, token);
+
+
+        //redirect our user to Imagelist component
+
+        router.push('/');
+    }
+
+}; 
+
+const mutations = {
+    setImages:(state,images)=>{
+        state.images = images;
+    }
+};
+
+export default{
+    state,
+    getters,
+    actions,
+    mutations
+};
